04d5608f6bcbefb5b93c36ed6c3da6ccf0286c70
Deok Filho <deokcelmofilho@gmail.com>
2020-09-13 10:28:31 -0500

    keeping track of Lambda Function code versioning

---
+++ find_meeting_time/index.js
@@ -0,0 +1,13 @@
+exports.handler = async (request) => {
+    const eventsList = request.items;
+    // TODO implement
+    // For each account
+        // Get Free Chunks
+            // 
+    // Match free chunks
+        // try to match everyone, provide best option if matching everyone not possible
+    const response = {
+        statusCode: 200
+    };
+    return response;
+};
